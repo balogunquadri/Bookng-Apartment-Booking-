651a4243411094f966eb18f8bd9a84cf8f2ce173
Phantonce <yes201045@gmail.com>
2023-04-30 20:23:47 +0100

    db updated

--- booking/app/layout.tsx
+++ booking/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
-
+import Log
 export const metadata = {
   title: 'Bookn',
   description: 'Book Your Next African Home',
@@ -25,6 +25,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
